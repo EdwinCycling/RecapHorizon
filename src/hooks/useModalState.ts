823fbe247886470e1cfaa678aff0d9adc0d6a6e5
Edwin <65449707+EdwinCycling@users.noreply.github.com>
2025-09-06 16:17:56 +0200

    Delete src directory

--- src/hooks/useModalState.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-export function useModalState(initial: boolean = false) {
-  const [isOpen, setIsOpen] = useState(initial);
-  const open = () => setIsOpen(true);
-  const close = () => setIsOpen(false);
-  return { isOpen, open, close };
-}

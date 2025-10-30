4525331d8abb2c12feb29358684e01d51d221bd1
Edwin <edwin@exact.com>
2025-10-30 19:25:54 +0100

    Updates

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Removed no-op fetch handler to prevent warning
+// The service worker will now only handle install and activate events
